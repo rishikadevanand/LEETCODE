219485876eb3b5df6445134fe0c85eea4b0950f7
Rishika Devanand <rishidev2107@gmail.com>
2026-07-14 14:45:30 +0530

    Create 283-MoveZeroes

---
+++ 283-MoveZeroes/283-MoveZeroes.java
@@ -0,0 +1,14 @@
+// Last updated: 7/14/2026, 2:45:29 PM
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j++;
+            }
+        } 
+    }
+}
