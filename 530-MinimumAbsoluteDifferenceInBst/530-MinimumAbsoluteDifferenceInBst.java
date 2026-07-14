6ab5cab2bc6433605ca50ef741304375ec8c3396
Rishika Devanand <rishidev2107@gmail.com>
2026-07-14 14:44:42 +0530

    Create 530-MinimumAbsoluteDifferenceInBst

---
+++ 530-MinimumAbsoluteDifferenceInBst/530-MinimumAbsoluteDifferenceInBst.java
@@ -0,0 +1,16 @@
+// Last updated: 7/14/2026, 2:44:41 PM
+class Solution {
+    int prev = Integer.MAX_VALUE;
+    int ans = Integer.MAX_VALUE;
+    public int getMinimumDifference(TreeNode root) {
+        inOrder(root);
+        return ans;
+    }
+    
+    public void inOrder(TreeNode root){
+        if(root.left!=null) inOrder(root.left);
+        ans = Math.min(ans,Math.abs(root.val-prev));
+        prev = root.val;
+        if(root.right!=null) inOrder(root.right);
+    }
+}
