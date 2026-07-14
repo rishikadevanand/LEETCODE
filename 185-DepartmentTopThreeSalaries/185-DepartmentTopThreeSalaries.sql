84dbc33bc126a11b062683deb71ee3de204fe455
Rishika Devanand <rishidev2107@gmail.com>
2026-07-14 14:46:20 +0530

    Create 185-DepartmentTopThreeSalaries

---
+++ 185-DepartmentTopThreeSalaries/185-DepartmentTopThreeSalaries.sql
@@ -0,0 +1,11 @@
+-- Last updated: 7/14/2026, 2:46:19 PM
+with cte as (
+    select d.name as Department, 
+    e.name as Employee, 
+    e.salary As Salary ,
+    dense_rank() over(partition by d.name order by e.salary desc ) as ranks
+from Employee as e
+left join Department as d
+on d.id = e.departmentId 
+) select Department,Employee,Salary from cte 
+where ranks <= 3;
