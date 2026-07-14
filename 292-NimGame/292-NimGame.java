e3f19f7cea8d392678258268f844edd11a9aa7ca
Rishika Devanand <rishidev2107@gmail.com>
2026-07-14 14:45:31 +0530

    Create 292-NimGame

---
+++ 292-NimGame/292-NimGame.java
@@ -0,0 +1,9 @@
+// Last updated: 7/14/2026, 2:45:30 PM
+class Solution {
+    public boolean canWinNim(int n) {
+        if(n%4==0)
+          return false;
+        return true;
+        
+    }
+}
