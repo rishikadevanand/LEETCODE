a88685bf775eb9d882cace01131a68527db7a109
Rishika Devanand <rishidev2107@gmail.com>
2026-07-14 14:47:57 +0530

    Create 71-SimplifyPath

---
+++ 71-SimplifyPath/71-SimplifyPath.java
@@ -0,0 +1,37 @@
+// Last updated: 7/14/2026, 2:47:56 PM
+import java.util.Stack;
+import java.util.Arrays;
+
+class Solution {
+    public String simplifyPath(String s) {
+        Stack<String> stack = new Stack<>();
+        
+        String[] components = s.split("/");
+        
+        for (String component : components) {
+            if (component.equals(".") || component.isEmpty()) {
+
+                continue;
+            } else if (component.equals("..")) {
+
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                }
+            } else {
+    
+                stack.push(component);
+            }
+        }
+        
+        if (stack.isEmpty()) {
+            return "/";
+        }
+        
+        StringBuilder ans = new StringBuilder();
+        for (String dir : stack) {
+            ans.append("/").append(dir);
+        }
+        
+        return ans.toString();
+    }
+}
