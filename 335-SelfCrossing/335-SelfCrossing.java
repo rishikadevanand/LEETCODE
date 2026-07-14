954733a53da022494f31bd1b5430c9cadf4cebae
Rishika Devanand <rishidev2107@gmail.com>
2026-07-14 14:45:19 +0530

    Create 335-SelfCrossing

---
+++ 335-SelfCrossing/335-SelfCrossing.java
@@ -0,0 +1,30 @@
+// Last updated: 7/14/2026, 2:45:18 PM
+public class Solution {
+    public boolean isSelfCrossing(int[] x) {
+        if (x.length <= 3) {
+            return false;
+        }
+        int i = 2;
+        // keep spiraling outward
+        while (i < x.length && x[i] > x[i - 2]) {
+            i++;
+        }
+        if (i >= x.length) {
+            return false;
+        }
+        // transition from spiraling outward to spiraling inward
+        if ((i >= 4 && x[i] >= x[i - 2] - x[i - 4]) ||
+                (i == 3 && x[i] == x[i - 2])) {
+            x[i - 1] -= x[i - 3];
+        }
+        i++;
+        // keep spiraling inward
+        while (i < x.length) {
+            if (x[i] >= x[i - 2]) {
+                return true;
+            }
+            i++;
+        }
+        return false;
+    }
+}
