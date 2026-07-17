458e22d273983a9583328b756ab0ab2e09e01254
Rishika Devanand <rishidev2107@gmail.com>
2026-07-17 15:12:35 +0530

    Create 626-ExchangeSeats

---
+++ 626-ExchangeSeats/626-ExchangeSeats.sql
@@ -0,0 +1,9 @@
+-- Last updated: 7/17/2026, 3:12:35 PM
+# Write your MySQL query statement below
+
+select case when id % 2 =1 and id+1 in (select id from Seat) then id+1
+            when id % 2 =0 then id-1
+            else id
+        end as id, student
+        from Seat
+        order by id;
